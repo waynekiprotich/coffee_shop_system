c4974eb594fb5946f849f440c84ef60c42b73074
Wayne Kiprotich <wanekip123@icloud.com>
2026-02-25 15:17:18 +0300

    added utils.py file

---
+++ main.py
@@ -0,0 +1,17 @@
+from auth import Auth
+from order import Order
+from storage import Storage
+from ut
+
+coffee_menu = {
+    "1": {"name": "Espresso", "price": 200},
+    "2": {"name": "Latte", "price": 250},
+    "3": {"name": "Cappuccino", "price": 300}
+}
+
+coffee_size = {
+    "small": 1,
+    "medium": 1.2,
+    "large": 1.5
+}
+
